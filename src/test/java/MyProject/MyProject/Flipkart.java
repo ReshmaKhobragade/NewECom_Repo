d63d24be6b637dc8980e7cc0a1174a30525b07b5
ReshmaKhobragade <reshmakhobragade93@gmail.com>
2023-08-04 18:13:09 +0530

    [Reshma] change in flipkart

--- src/test/java/MyProject/MyProject/Flipkart.java
+++ src/test/java/MyProject/MyProject/Flipkart.java
@@ -6,12 +6,13 @@ import org.testng.annotations.Test;
 
 public class Flipkart {
   @Test
-  public void FlipkartTest()
+  public void FlipkartTest() throws InterruptedException
   {
 	  WebDriver driver = new ChromeDriver();
 	  driver.get("https://www.flipkart.com/");
 	  driver.manage().window().maximize();
 	  driver.manage().deleteAllCookies();
+	  Thread.sleep(3000);
 	  driver.close();
   }
 }
